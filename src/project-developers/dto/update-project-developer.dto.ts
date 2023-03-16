b6439d2a549049b04ed55a30418232dc97de0484
Sebastjan Mevlja <38669826+mevljas@users.noreply.github.com>
2023-03-16 22:53:43 +0100

    fix merge conflicts

--- src/project-developers/dto/update-project-developer.dto.ts
+++ src/project-developers/dto/update-project-developer.dto.ts
@@ -1,10 +1,6 @@
 import { IsNotEmpty, IsNumber } from 'class-validator';
 
-<<<<<<< HEAD:src/project-developers/dto/project-developer.dto.ts
 export class ProjectDeveloperDto {
-=======
-export class UpdateProjectDeveloperDto {
->>>>>>> master:src/project-developers/dto/update-project-developer.dto.ts
   @IsNotEmpty()
   @IsNumber()
   userId: number;
