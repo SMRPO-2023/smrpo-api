e73a0c224305c2e58ecfa578e5fd5caa00bd7494
Sebastjan Mevlja <sm9299@student.uni-lj.si>
2023-03-24 23:19:12 +0100

    Revert "attempt to fix acceptanceTest"
    This reverts commit 3947a937152438a3d0020fc29c7e2c55b949efb8.

--- src/user-stories/dto/accept-user-story.dto.ts
+++ src/user-stories/dto/accept-user-story.dto.ts
@@ -1,8 +1,6 @@
-import { Transform, Type } from 'class-transformer';
 import { IsBoolean } from 'class-validator';
 
 export class AcceptUserStoryDto {
   @IsBoolean()
-  @Type(() => Boolean)
   acceptanceTest: boolean;
 }
