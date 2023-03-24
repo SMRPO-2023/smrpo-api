3947a937152438a3d0020fc29c7e2c55b949efb8
Sebastjan Mevlja <sm9299@student.uni-lj.si>
2023-03-24 23:14:12 +0100

    attempt to fix acceptanceTest

--- src/user-stories/dto/accept-user-story.dto.ts
+++ src/user-stories/dto/accept-user-story.dto.ts
@@ -1,6 +1,8 @@
+import { Transform, Type } from 'class-transformer';
 import { IsBoolean } from 'class-validator';
 
 export class AcceptUserStoryDto {
   @IsBoolean()
+  @Type(() => Boolean)
   acceptanceTest: boolean;
 }
