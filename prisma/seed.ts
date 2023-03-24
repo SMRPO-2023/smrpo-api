1991f46b37fa08b5e1987005273161ca98c1c59c
Sebastjan Mevlja <sm9299@student.uni-lj.si>
2023-03-24 20:29:40 +0100

    seed improvement

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,24 +1,39 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, StoryPriority } from '@prisma/client';
+import { faker } from '@faker-js/faker';
 
 const prisma = new PrismaClient();
 
+function getPriority(i: number) {
+  switch (i) {
+    case 0:
+      return StoryPriority.MUST_HAVE;
+    case 1:
+      return StoryPriority.COULD_HAVE;
+    case 2:
+      return StoryPriority.SHOULD_HAVE;
+    case 3:
+      return StoryPriority.WONT_HAVE;
+  }
+}
+
 async function main() {
+  console.log('Clearing the database...');
+
+  await prisma.userStory.deleteMany();
+  await prisma.sprint.deleteMany();
+  await prisma.projectDeveloper.deleteMany();
+  await prisma.project.deleteMany();
   await prisma.user.deleteMany();
+  await prisma.post.deleteMany();
+  await prisma.storyComment.deleteMany();
+  await prisma.task.deleteMany();
+  await prisma.timeLog.deleteMany();
 
   console.log('Seeding...');
 
-  const user1 = await prisma.user.create({
-    data: {
-      email: 'lisa@simpson.com',
-      firstname: 'Lisa',
-      lastname: 'Simpson',
-      // cspell:disable-next-line -- disables checking till the end of the next line.
-      password: '$2b$10$DRzCId0X0guJa7wtynJ0FOrAijm7IY9l2Ora9KygCK4lwH1lSvV12', // secret12345678
-      role: 'USER',
-      username: 'simpsonl',
-    },
-  });
-  const user2 = await prisma.user.create({
+  // Generate users.
+
+  const simpsonb = await prisma.user.create({
     data: {
       email: 'bart@simpson.com',
       firstname: 'Bart',
@@ -30,268 +45,242 @@ async function main() {
     },
   });
 
-  const user3 = await prisma.user.create({
+  const simpsonl = await prisma.user.create({
     data: {
-      email: 'user@gmail.com',
-      firstname: 'firstname',
-      lastname: 'lastname',
-      role: 'ADMIN',
+      email: 'lisa@simpson.com',
+      firstname: 'Lisa',
+      lastname: 'Simpson',
       // cspell:disable-next-line -- disables checking till the end of the next line.
       password: '$2b$10$DRzCId0X0guJa7wtynJ0FOrAijm7IY9l2Ora9KygCK4lwH1lSvV12', // secret12345678
-      username: 'user',
+      role: 'USER',
+      username: 'simpsonl',
     },
   });
 
+  const ferrella = await prisma.user.create({
+    data: {
+      email: 'angelique.ferrell@gmail.com',
+      firstname: 'Angelique',
+      lastname: 'Ferrell',
+      role: 'USER',
+      // cspell:disable-next-line -- disables checking till the end of the next line.
+      password: '$2b$10$DRzCId0X0guJa7wtynJ0FOrAijm7IY9l2Ora9KygCK4lwH1lSvV12', // secret12345678
+      username: 'ferrella',
+    },
+  });
+
+  const hernandezc = await prisma.user.create({
+    data: {
+      email: 'concetta.hernandez@gmail.com',
+      firstname: 'Concetta',
+      lastname: 'Hernandez',
+      role: 'USER',
+      // cspell:disable-next-line -- disables checking till the end of the next line.
+      password: '$2b$10$DRzCId0X0guJa7wtynJ0FOrAijm7IY9l2Ora9KygCK4lwH1lSvV12', // secret12345678
+      username: 'hernandezc',
+    },
+  });
+
+  const jacksonm = await prisma.user.create({
+    data: {
+      email: 'micahel.jackson@gmail.com',
+      firstname: 'Michael',
+      lastname: 'Jackson',
+      role: 'USER',
+      // cspell:disable-next-line -- disables checking till the end of the next line.
+      password: '$2b$10$DRzCId0X0guJa7wtynJ0FOrAijm7IY9l2Ora9KygCK4lwH1lSvV12', // secret12345678
+      username: 'jacksonm',
+    },
+  });
+
+  const bushg = await prisma.user.create({
+    data: {
+      email: '911@gmail.com',
+      firstname: 'George',
+      lastname: 'Bush',
+      role: 'USER',
+      // cspell:disable-next-line -- disables checking till the end of the next line.
+      password: '$2b$10$DRzCId0X0guJa7wtynJ0FOrAijm7IY9l2Ora9KygCK4lwH1lSvV12', // secret12345678
+      username: 'bushg',
+    },
+  });
+
+  // Generate projects
+
   const project1 = await prisma.project.create({
     data: {
-      title: 'project 1',
-      documentation: 'project 1 documentation',
-      projectOwnerId: user2.id,
-      scrumMasterId: user2.id,
+      title: 'Project 1',
+      documentation: faker.random.words(20),
+      projectOwnerId: simpsonl.id,
+      scrumMasterId: ferrella.id,
     },
   });
 
   const project2 = await prisma.project.create({
     data: {
       title: 'project 2',
-      documentation: 'project 2 documentation',
-      projectOwnerId: user3.id,
-      scrumMasterId: user1.id,
+      documentation: 'project 2 documentation.',
+      projectOwnerId: ferrella.id,
+      scrumMasterId: simpsonl.id,
     },
   });
 
   const project3 = await prisma.project.create({
     data: {
-      title: 'project 3',
-      documentation: 'project 3 documentation',
-      projectOwnerId: user2.id,
-      scrumMasterId: user3.id,
+      title: 'Inside job',
+      documentation: 'Jet fuel cant melt steel beams.',
+      projectOwnerId: bushg.id,
+      scrumMasterId: jacksonm.id,
     },
   });
 
   await prisma.projectDeveloper.create({
     data: {
-      userId: user1.id,
       projectId: project1.id,
+      userId: ferrella.id,
     },
   });
 
   await prisma.projectDeveloper.create({
     data: {
-      userId: user1.id,
+      projectId: project1.id,
+      userId: jacksonm.id,
+    },
+  });
+
+  await prisma.projectDeveloper.create({
+    data: {
+      projectId: project1.id,
+      userId: bushg.id,
+    },
+  });
+
+  await prisma.projectDeveloper.create({
+    data: {
       projectId: project2.id,
+      userId: simpsonl.id,
     },
   });
 
   await prisma.projectDeveloper.create({
     data: {
-      userId: user1.id,
+      projectId: project2.id,
+      userId: simpsonb.id,
+    },
+  });
+
+  await prisma.projectDeveloper.create({
+    data: {
       projectId: project3.id,
+      userId: jacksonm.id,
     },
   });
 
   await prisma.projectDeveloper.create({
     data: {
-      userId: user2.id,
-      projectId: project1.id,
-    },
-  });
-
-  await prisma.projectDeveloper.create({
-    data: {
-      userId: user2.id,
-      projectId: project2.id,
-    },
-  });
-
-  await prisma.projectDeveloper.create({
-    data: {
-      userId: user3.id,
-      projectId: project1.id,
-    },
-  });
-
-  await prisma.projectDeveloper.create({
-    data: {
-      userId: user3.id,
       projectId: project3.id,
+      userId: simpsonb.id,
     },
   });
 
-  const sprint1 = await prisma.sprint.create({
+  await prisma.projectDeveloper.create({
     data: {
-      start: new Date('2023-03-17T21:43:28.434Z'),
-      end: new Date('2023-03-14T21:43:28.434Z'),
-      velocity: 5,
-      projectId: project1.id,
-      name: 'sprint 1',
-    },
-  });
-
-  const sprint2 = await prisma.sprint.create({
-    data: {
-      start: new Date('2023-04-17T21:43:28.434Z'),
-      end: new Date('2023-04-22T21:43:28.434Z'),
-      velocity: 10,
-      projectId: project1.id,
-      name: 'sprint 2',
-    },
-  });
-
-  const sprint3 = await prisma.sprint.create({
-    data: {
-      start: new Date('2023-05-17T21:43:28.434Z'),
-      end: new Date('2023-05-22T21:43:28.434Z'),
-      velocity: 15,
-      projectId: project1.id,
-      name: 'sprint 3',
-    },
-  });
-
-  await prisma.sprint.create({
-    data: {
-      start: new Date('2023-01-11T21:43:28.434Z'),
-      end: new Date('2023-01-18T21:43:28.434Z'),
-      velocity: 9,
-      projectId: project2.id,
-      name: 'sprint 4',
-    },
-  });
-
-  await prisma.sprint.create({
-    data: {
-      start: new Date('2023-02-01T21:43:28.434Z'),
-      end: new Date('2023-02-15T21:43:28.434Z'),
-      velocity: 23,
-      projectId: project2.id,
-      name: 'sprint 5',
-    },
-  });
-
-  await prisma.sprint.create({
-    data: {
-      start: new Date('2023-03-12T21:43:28.434Z'),
-      end: new Date('2023-03-22T21:43:28.434Z'),
-      velocity: 20,
-      projectId: project2.id,
-      name: 'sprint 6',
-    },
-  });
-
-  const story1 = await prisma.userStory.create({
-    data: {
-      priority: 'MUST_HAVE',
-      title: 'story 1',
-      description: 'story 1 description',
-      points: 3,
-      businessValue: 0,
       projectId: project3.id,
-      sprintId: sprint1.id,
-      acceptanceCriteria: 'acceptanceCriteria',
+      userId: simpsonl.id,
     },
   });
 
-  const story2 = await prisma.userStory.create({
-    data: {
-      priority: 'MUST_HAVE',
-      title: 'story 2',
-      description: 'story 2 description',
-      points: 3,
-      businessValue: 1,
-      projectId: project2.id,
-      sprintId: sprint2.id,
-      acceptanceCriteria: 'acceptanceCriteria',
-    },
-  });
+  const projects = [project1, project2, project3];
 
-  const story3 = await prisma.userStory.create({
-    data: {
-      priority: 'MUST_HAVE',
-      title: 'story 3',
-      description: 'story 3 description',
-      points: 7,
-      businessValue: 5,
-      projectId: project1.id,
-      sprintId: sprint3.id,
-      acceptanceCriteria: 'acceptanceCriteria',
-    },
-  });
+  for (let i = 0; i < projects.length; i++) {
+    const project = projects[i];
+    const sprints = [];
+    sprints.push(
+      await prisma.sprint.create({
+        data: {
+          start: new Date('2023-02-01T21:43:28.434Z'),
+          end: new Date('2023-02-10T21:43:28.434Z'),
+          velocity: 5,
+          projectId: project.id,
+          name: 'Sprint 1',
+        },
+      })
+    );
+    sprints.push(
+      await prisma.sprint.create({
+        data: {
+          start: new Date('2023-02-10T21:43:28.434Z'),
+          end: new Date('2023-02-20T21:43:28.434Z'),
+          velocity: 10,
+          projectId: project.id,
+          name: 'sprint 2',
+        },
+      })
+    );
+    sprints.push(
+      await prisma.sprint.create({
+        data: {
+          start: new Date('2023-02-20T21:43:28.434Z'),
+          end: new Date('2023-03-01T21:43:28.434Z'),
+          velocity: 15,
+          projectId: project.id,
+          name: 'sprint 3',
+        },
+      })
+    );
+    sprints.push(
+      await prisma.sprint.create({
+        data: {
+          start: new Date('2023-03-10T21:43:28.434Z'),
+          end: new Date('2023-03-20T21:43:28.434Z'),
+          velocity: 9,
+          projectId: project.id,
+          name: 'sprint 4',
+        },
+      })
+    );
+    sprints.push(
+      await prisma.sprint.create({
+        data: {
+          start: new Date('2023-04-01T21:43:28.434Z'),
+          end: new Date('2023-04-10T21:43:28.434Z'),
+          velocity: 23,
+          projectId: project.id,
+          name: 'sprint 5',
+        },
+      })
+    );
+    sprints.push(
+      await prisma.sprint.create({
+        data: {
+          start: new Date('2023-04-20T21:43:28.434Z'),
+          end: new Date('2023-05-01T21:43:28.434Z'),
+          velocity: 20,
+          projectId: project.id,
+          name: 'sprint 6',
+        },
+      })
+    );
 
-  const story4 = await prisma.userStory.create({
-    data: {
-      priority: 'MUST_HAVE',
-      title: 'story 4',
-      description: 'story 4 description',
-      points: 7,
-      businessValue: 5,
-      projectId: project1.id,
-      acceptanceCriteria: 'acceptanceCriteria',
-    },
-  });
-
-  const story5 = await prisma.userStory.create({
-    data: {
-      priority: 'MUST_HAVE',
-      title: 'story 5',
-      description: 'story 5 description',
-      points: 7,
-      businessValue: 5,
-      projectId: project1.id,
-      acceptanceCriteria: 'acceptanceCriteria',
-    },
-  });
-
-  const story6 = await prisma.userStory.create({
-    data: {
-      priority: 'MUST_HAVE',
-      title: 'story 6',
-      description: 'story 6 description',
-      points: 7,
-      businessValue: 5,
-      projectId: project1.id,
-      sprintId: sprint3.id,
-      acceptanceCriteria: 'acceptanceCriteria',
-    },
-  });
-
-  const story7 = await prisma.userStory.create({
-    data: {
-      priority: 'MUST_HAVE',
-      title: 'story 7',
-      description: 'story 7 description',
-      points: 7,
-      businessValue: 5,
-      projectId: project1.id,
-      sprintId: sprint3.id,
-      acceptanceCriteria: 'acceptanceCriteria',
-    },
-  });
-
-  const story8 = await prisma.userStory.create({
-    data: {
-      priority: 'MUST_HAVE',
-      title: 'story 7',
-      description: 'story 7 description',
-      points: 7,
-      businessValue: 5,
-      projectId: project1.id,
-      sprintId: sprint3.id,
-      acceptanceCriteria: 'acceptanceCriteria',
-    },
-  });
-
-  const story9 = await prisma.userStory.create({
-    data: {
-      priority: 'MUST_HAVE',
-      title: 'story 7',
-      description: 'story 7 description',
-      points: 7,
-      businessValue: 5,
-      projectId: project1.id,
-      sprintId: sprint3.id,
-      acceptanceCriteria: 'acceptanceCriteria',
-    },
-  });
+    for (let j = 0; j < sprints.length; j++) {
+      const sprint = sprints[j];
+      for (let z = 0; z < 5; z++) {
+        await prisma.userStory.create({
+          data: {
+            priority: getPriority(+faker.random.numeric() % 4),
+            title: 'Story ' + i,
+            description: faker.random.words(20),
+            points: +faker.datatype.number({ min: 4, max: 15 }),
+            businessValue: +faker.datatype.number({ min: 1, max: 7 }),
+            projectId: sprint.projectId,
+            sprintId: sprint.id,
+            acceptanceCriteria: faker.random.words(10),
+            acceptanceTest: i % 2 == 0 ? true : false,
+          },
+        });
+      }
+    }
+  }
 
   console.log('Seeding finished.');
 }
