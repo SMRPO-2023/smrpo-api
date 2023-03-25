f77dbe9907215f0a34e319cf3bf1a6eb601c7ab8
Luka Galjot <luka.galjot@gmail.com>
2023-03-25 12:31:15 +0100

    Hope version works after build

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,10 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
-  }
-
-  getHelloName(name: string): string {
-    return `Hello ${name}!`;
+    return 'Hello to SMRPO api ' + process.env.npm_package_version;
   }
 }
