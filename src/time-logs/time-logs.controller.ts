d3493c68272aefc6c47ce7a4b02946cab92d6e3f
Sebastjan Mevlja <sm9299@student.uni-lj.si>
2023-04-09 11:16:06 +0200

    add time logging endpoints

--- src/time-logs/time-logs.controller.ts
+++ src/time-logs/time-logs.controller.ts
@@ -3,45 +3,54 @@ import {
   Get,
   Post,
   Body,
-  Patch,
   Param,
   Delete,
   ParseIntPipe,
+  UseGuards,
+  Query,
+  Put,
 } from '@nestjs/common';
 import { TimeLogsService } from './time-logs.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiQuery, ApiTags } from '@nestjs/swagger';
 import { TimeLogDto } from './dto/time-log.dto';
+import { UserEntity } from 'src/common/decorators/user.decorator';
+import { User } from '@prisma/client';
+import { JwtAuthGuard } from 'src/auth/jwt-auth-guard.service';
 
 @Controller('time-logs')
 @ApiTags('Time logs')
+@UseGuards(JwtAuthGuard)
+@ApiBearerAuth()
 export class TimeLogsController {
   constructor(private readonly timeLogsService: TimeLogsService) {}
 
-  @Post()
-  create(@Body() timeLogDto: TimeLogDto) {
-    return this.timeLogsService.create(timeLogDto);
+  @Post('log-hours')
+  logHours(@Body() timeLogDto: TimeLogDto, @UserEntity() user: User) {
+    return this.timeLogsService.logHours(timeLogDto, user);
   }
 
   @Get()
-  findAll() {
-    return this.timeLogsService.findAll();
+  @ApiQuery({ name: 'task-id', required: false, type: Number })
+  @ApiQuery({ name: 'user-id', required: false, type: Number })
+  findAll(
+    @UserEntity() user: User,
+    @Query('task-id') taskId?: number,
+    @Query('user-id') userId?: number
+  ) {
+    return this.timeLogsService.findAll(user, taskId, userId);
   }
 
-  @Get(':id')
-  findOne(@Param('id', ParseIntPipe) id: number) {
-    return this.timeLogsService.findOne(+id);
-  }
-
-  @Patch(':id')
+  @Put(':id')
   update(
+    @UserEntity() user: User,
     @Param('id', ParseIntPipe) id: number,
     @Body() timeLogDto: TimeLogDto
   ) {
-    return this.timeLogsService.update(+id, timeLogDto);
+    return this.timeLogsService.update(+id, timeLogDto, user);
   }
 
   @Delete(':id')
-  remove(@Param('id', ParseIntPipe) id: number) {
-    return this.timeLogsService.remove(+id);
+  remove(@UserEntity() user: User, @Param('id', ParseIntPipe) id: number) {
+    return this.timeLogsService.remove(+id, user);
   }
 }
